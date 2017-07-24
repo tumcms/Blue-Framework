c6b5b1018532b7c0dca9c72213d33883feb4cc5a
Julian Amann <julian.amann@tum.de>
2017-07-24 13:20:55 +0200

    Removed subimage function from unit test.

--- UnitTests/src/BlueFramework/UnitTests/ImageProcessing/RandomHeightmapGeneration/randomHeightmapGeneration.cpp
+++ UnitTests/src/BlueFramework/UnitTests/ImageProcessing/RandomHeightmapGeneration/randomHeightmapGeneration.cpp
@@ -23,29 +23,11 @@
 #include <BlueFramework/ImageProcessing/captureScreen.h>
 #include <BlueFramework/ImageProcessing/io.h>
 #include <BlueFramework/ImageProcessing/proceduralTextureGeneration.h>
-#include <BlueFramework/ImageProcessing/findSubimage.h>
 
 #include <gtest/gtest.h>
 #include <iostream>
-#include <string>
 
-#include <Windows.h>
-
-TEST(ImageProcessing, RandomHeightmapGeneration) {
-/*
-	// buw::captureScreen("C:/temp/screenshot.bmp");
-	buw::Image3b img = buw::loadImage3b("C:/temp/screenshot.bmp");
-	buw::Image3b subimage = buw::loadImage3b("C:/temp/subimage.png");
-
-	int x = 0, y = 0;
-	if (buw::findSubimage("C:/temp/subimage.png", "C:/temp/screenshot.bmp", x, y)) {
-		buw::drawRect(x, y, subimage.getWidth(), subimage.getHeight(), img);
-		buw::storeImage("C:/temp/findResult.bmp", img);
-	}
-
-	return;
-*/
-
+TEST(ImageProcessing, RandomHeightmapGeneration) {	
 	buw::Image4b heightmap = buw::generateRandomHeightmap(10, 0.5f, 0.0f, 1.0f);
 
 	buw::Image4b filtered = buw::applyGaussian<uint8_t, 4Ui64, buw::eFilterSize::Medium>(heightmap, buw::Matrix22f::Identity());
