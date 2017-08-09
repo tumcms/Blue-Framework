0ceca3c3476b9a59d99823e34fddaf048be6e768
Julian Amann <julian.amann@tum.de>
2017-08-09 12:59:23 +0200

    Added simple std::map Wrapper.

---
+++ Core/src/BlueFramework/Core/Map.h
@@ -0,0 +1,46 @@
+/*
+    This file is part of BlueFramework, a simple 3D engine.
+    Copyright (c) 2016-2017 Technical University of Munich
+    Chair of Computational Modeling and Simulation.
+
+    BlueFramework is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    BlueFramework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#ifndef BlueFramework_Core_Map_6b14d240_f47b_4627_9021_abaacefee21b_h
+#define BlueFramework_Core_Map_6b14d240_f47b_4627_9021_abaacefee21b_h
+
+#include <BlueFramework/Core/namespace.h>
+#include <map>
+
+BLUEFRAMEWORK_CORE_NAMESPACE_BEGIN
+
+// A simple std::map wrapper.
+// For some reason the C++ committee did never manage to create a easy usable map.
+template <typename Key, typename Value>
+class Map : public std::map<Key, Value> {
+public:
+	bool contains(const Key& key) {
+		return this->find(key) != this->end();
+	}
+
+	void insert(const Key& k, const Value& v) {
+		(*this)[k] = v;
+	}
+};
+
+BLUEFRAMEWORK_CORE_NAMESPACE_END
+
+BLUEFRAMEWORK_CORE_EMBED_INTO_BUW_NAMESPACE(Map)
+
+#endif // end define BlueFramework_Core_Map_6b14d240_f47b_4627_9021_abaacefee21b_h
