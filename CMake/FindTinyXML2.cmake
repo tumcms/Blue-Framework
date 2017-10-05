604a5e44ccd1f961a03c54a6d74452e3b002391f
Helge Hecht <helge.hecht@tum.de>
2017-10-05 16:39:03 +0200

    Finished VS2017 support for external build and CMake scripts.

--- CMake/FindTinyXML2.cmake
+++ CMake/FindTinyXML2.cmake
@@ -29,14 +29,26 @@ if(NOT TINYXML2_ROOT)
 	option(TINYXML2_AUTOMATIC_INSTALL OFF)
 	if(TINYXML2_AUTOMATIC_INSTALL AND TINYXML2_INSTALL_DIR)
 		message(STATUS "Installing TinyXML2...")
-		execute_process(COMMAND "${PROJECT_SOURCE_DIR}/external/Build_tinyxml2_Visual Studio 14 2015 Win64.cmd"
-		${TINYXML2_INSTALL_DIR}
-		"${CMAKE_COMMAND}"
-		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/external
-		RESULT_VARIABLE RESULT
-		ERROR_FILE "${PROJECT_SOURCE_DIR}/external/log_install_tinyxml2.txt"
-		OUTPUT_FILE "${PROJECT_SOURCE_DIR}/external/log_install_tinyxml2.txt"
-		)
+		if(${MSVC_VERSION_STRING} STREQUAL "vs2015")
+			execute_process(COMMAND "${PROJECT_SOURCE_DIR}/external/Build_tinyxml2_Visual Studio 14 2015 Win64.cmd"
+				${TINYXML2_INSTALL_DIR}
+				"${CMAKE_COMMAND}"
+				WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/external
+				RESULT_VARIABLE RESULT
+				ERROR_FILE "${PROJECT_SOURCE_DIR}/external/log_install_tinyxml2.txt"
+				OUTPUT_FILE "${PROJECT_SOURCE_DIR}/external/log_install_tinyxml2.txt")
+		elseif(${MSVC_VERSION_STRING} STREQUAL "vs2017")
+			execute_process(COMMAND "${PROJECT_SOURCE_DIR}/external/Build_tinyxml2_Visual Studio 15 2017 Win64.cmd"
+				${TINYXML2_INSTALL_DIR}
+				"${CMAKE_COMMAND}"
+				WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/external
+				RESULT_VARIABLE RESULT
+				ERROR_FILE "${PROJECT_SOURCE_DIR}/external/log_install_tinyxml2.txt"
+				OUTPUT_FILE "${PROJECT_SOURCE_DIR}/external/log_install_tinyxml2.txt")
+		else()
+			message(FATAL_ERROR "Couldn't determine VS version.")
+		endif()
+
 		if(RESULT EQUAL 0)
 			message(STATUS "Successfully installed TinyXML2.")
 			set(TINYXML2_ROOT ${TINYXML2_INSTALL_DIR}/tinyxml2-master CACHE PATH "Tinyxml2 root" FORCE)
