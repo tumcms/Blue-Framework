2bbe97f92299413831fb1e1ca9c198ee38624505
Julian Amann <julian.amann@tum.de>
2018-06-22 08:20:55 +0200

    Removed unreferenced local variables in D3D11 sampler. Warning count from D3D11 render system is no 10 warnings less.

--- D3D11RenderSystem/src/BlueFramework/D3D11RenderSystem/D3D11Sampler.cpp
+++ D3D11RenderSystem/src/BlueFramework/D3D11RenderSystem/D3D11Sampler.cpp
@@ -29,17 +29,6 @@ D3D11Sampler::D3D11Sampler(D3D11RenderSystem* renderSystem, buw::samplerStateDes
 	ISampler(renderSystem, ssd),
 	renderSystem_(renderSystem)
 {
-	D3D11_FILTER Filter;
-	D3D11_TEXTURE_ADDRESS_MODE AddressU;
-	D3D11_TEXTURE_ADDRESS_MODE AddressV;
-	D3D11_TEXTURE_ADDRESS_MODE AddressW;
-	FLOAT MipLODBias;
-	UINT MaxAnisotropy;
-	D3D11_COMPARISON_FUNC ComparisonFunc;
-	FLOAT BorderColor[4];
-	FLOAT MinLOD;
-	FLOAT MaxLOD;
-
 	D3D11_SAMPLER_DESC desc = {};
 	desc.Filter = mapFilter(ssd);
 	desc.AddressU = mapTextureAddressMode(ssd.textureAddressModeU);
