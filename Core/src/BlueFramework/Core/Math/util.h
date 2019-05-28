0a48cd89c5c74776399205b8783f99644cff30d2
Julian Amann <Julian Amann@localhost>
2019-05-28 15:34:19 +0200

    Fix compile errors in util.h

--- Core/src/BlueFramework/Core/Math/util.h
+++ Core/src/BlueFramework/Core/Math/util.h
@@ -16,11 +16,14 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "BlueFramework/Core/namespace.h"
-
 #pragma once
 #ifndef BlueFramework_Core_Math_util_6b3f6d1c_d23a_4641_b619_b13fbd3aa260_h
 #define BlueFramework_Core_Math_util_6b3f6d1c_d23a_4641_b619_b13fbd3aa260_h
+
+#include "BlueFramework/Core/namespace.h"
+
+#include <limits>
+#include <cmath>
 
 BLUEFRAMEWORK_CORE_MATH_NAMESPACE_BEGIN
 
