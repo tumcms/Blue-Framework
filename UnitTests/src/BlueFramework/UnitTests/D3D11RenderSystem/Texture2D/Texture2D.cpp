6c236ed60d7e60f6c8465e8fcb282fc7c4f03689
Helge Hecht <helge.hecht@gmx.de>
2020-08-06 12:44:56 +0200

    Implemented UnitTest for mutlisampled textures
    Added unit test to test multisampling behavior of texture and downloading the image from it

--- UnitTests/src/BlueFramework/UnitTests/D3D11RenderSystem/Texture2D/Texture2D.cpp
+++ UnitTests/src/BlueFramework/UnitTests/D3D11RenderSystem/Texture2D/Texture2D.cpp
@@ -16,25 +16,54 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <gtest/gtest.h>
 #include <BlueFramework/D3D11RenderSystem/D3D11RenderSystem.h>
+#include <gtest/gtest.h>
 
 using namespace testing;
 using namespace BlueFramework::D3D11RenderSystem;
 
-class D3D11Texture2DTest : public Test {
-
+class ITexture2DBuilder {
+	
+	buw::texture2DDescription _desc = buw::texture2DDescription(1,1, buw::eTextureFormat::R8G8B8A8_UnsignedNormalizedInt_SRGB);
+	buw::eTextureBindType _bindType = BlueFramework::Rasterizer::eTextureBindType::None;
+	buw::ReferenceCounted<buw::IRenderSystem> _renderSystem = nullptr;
+	
 public:
+	buw::ReferenceCounted<buw::ITexture2D> Build() {
+		return _renderSystem->createTexture2D(_desc, _bindType);
+	}
 
+	ITexture2DBuilder WithMSAA() {
+		_desc.useMSAA = true;
+		return *this;
+	}
+
+	ITexture2DBuilder WithDescription(const BlueFramework::Rasterizer::texture2DDescription& desc) {
+		_desc = desc;
+		return *this;
+	}
+
+	ITexture2DBuilder MakeCPUReadable() {
+		_desc.isCpuReadable = true;
+		return *this;
+	}
+
+	ITexture2DBuilder(const buw::ReferenceCounted<D3D11RenderSystem> & renderSystem) : _renderSystem{renderSystem} {
+		
+	}
+};
+
+class D3D11Texture2DTest : public Test {
+public:
 	D3D11Texture2DTest() {
 		_renderSystemDescription.renderAPI = BlueFramework::Rasterizer::eRenderAPI::Direct3D11;
 		_renderSystemDescription.width = 640;
 		_renderSystemDescription.height = 480;
 		_renderSystemDescription.windowId = (void*)this;
-		
+
 		_renderSystem = buw::makeReferenceCounted<D3D11RenderSystem>(_renderSystemDescription);
 	}
-	
+
 	~D3D11Texture2DTest() override {
 		_renderSystem.reset();
 	}
@@ -44,7 +73,6 @@ protected:
 	buw::ReferenceCounted<D3D11RenderSystem> _renderSystem = nullptr;
 
 	void SetUp() override {
-
 	}
 
 	void TearDown() override {
@@ -52,17 +80,58 @@ protected:
 };
 
 TEST_F(D3D11Texture2DTest, CreateTexture2D_AfterConstruction_IsNotNull) {
-	//Arrange
-	buw::texture2DDescription desc = buw::texture2DDescription(
-            100,
-            100,
-            BlueFramework::Rasterizer::eTextureFormat::R8G8B8A8_UnsignedNormalizedInt_SRGB);
+	// Arrange
 	buw::ReferenceCounted<BlueFramework::Rasterizer::ITexture2D> texture = nullptr;
-
+	buw::texture2DDescription desc = buw::texture2DDescription(100, 100, BlueFramework::Rasterizer::eTextureFormat::R8G8B8A8_UnsignedNormalizedInt_SRGB);
+		
 	// Act, Assert
-	EXPECT_NO_THROW(texture = _renderSystem->createTexture2D(desc, buw::eTextureBindType::None));
+	EXPECT_NO_THROW(texture = ITexture2DBuilder(_renderSystem).WithDescription(desc).Build());
 	EXPECT_FALSE(texture == nullptr);
 
 	// Annihilate
 	texture.reset();
 }
+
+TEST_F(D3D11Texture2DTest, CreateWithMSAA_AfterCreation_HasMSAAEnabled) {
+	// Arrange
+	auto texture = ITexture2DBuilder(_renderSystem)
+		.WithMSAA()
+		.Build();
+
+	// Act, Assert
+	EXPECT_TRUE(texture->isMultisampled());
+
+	// Annihilate
+	texture.reset();
+}
+
+TEST_F(D3D11Texture2DTest, TextureWithMSAA_MakeCPUReadable_IsCPUReadable) {
+	// Arrange
+	auto texture = ITexture2DBuilder(_renderSystem)
+		.WithMSAA()
+		.MakeCPUReadable()
+		.Build();
+
+	// Act, Assert
+	EXPECT_TRUE(texture->isCPUReadable());
+
+	// Annihilate
+	texture.reset();
+}
+
+TEST_F(D3D11Texture2DTest, TextureWithMSAA_DownloadTexture_NoFailure) {
+	// Arrange
+	auto texture = ITexture2DBuilder(_renderSystem)
+		.WithMSAA()
+		.MakeCPUReadable()
+		.Build();
+
+	buw::Image4b image = buw::Image4b(texture->width(), texture->height());
+	
+	// Act, Assert
+	EXPECT_NO_THROW(_renderSystem->downloadTexture(texture, image));
+
+	// Annihilate
+	texture.reset();
+}
+
